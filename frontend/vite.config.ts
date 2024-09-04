5856af9cd03c19ebbff26a63e42ee0df108287d0
Max Schneider <maxwjs03@gmail.com>
2024-09-03 22:00:15 -0400

    changed to vite pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Ithildin',
+      short_name: 'Ithildin',
+      description: 'Password manager',
+      theme_color: '#ffffff'
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
